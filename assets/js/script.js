c3ead2aae48b543d9b6155faa9829879bf96b129
CoderGirl <angi.adema@gmail.com>
2023-04-12 15:55:05 -0600

    updated date picker in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
-// var time = moment().format('hh:mm:ss');
-// $('#time').text(time);
-
+// Date and time on hero banner.
 var datetime = null,
-        date = null;
+  date = null;
 
 var update = function () {
-    date = moment(new Date())
-    datetime.html(date.format('ddd, MMMM Do YYYY, h:mm:ss a'));
+  date = moment(new Date());
+  datetime.html(date.format("ddd, MMMM Do YYYY, h:mm:ss a"));
 };
 
-$(document).ready(function() {
-    datetime = $('#datetime')
-    update();
-    setInterval(update, 1000);
+$(document).ready(function () {
+  datetime = $("#datetime");
+  update();
+  setInterval(update, 1000);
+});
+
+// Date picker in modal.
+$(function () {
+  $("#due-date").datepicker({ minDate: 1 });
 });
