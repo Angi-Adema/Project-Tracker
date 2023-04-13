7b2fa94ce50f8ff56e0afd23078fe0e0569ad273
CoderGirl <angi.adema@gmail.com>
2023-04-13 13:18:32 -0600

    updated table print to screen data in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-var modelEl = $('#project-modal');
-
+var modelEl = $("#project-modal");
+var formEl = $("#form");
+var nameEl = $("#project-name");
+var projectTypeEl = $("#project-type");
+var rateEl = $("#hourly-rate");
+var dueDateEl = $("#due-date");
+var tableDisplayEl = $("#display");
 
 // Date and time on hero banner.
 var datetime = null,
@@ -15,6 +20,37 @@ $(document).ready(function () {
   update();
   setInterval(update, 1000);
 });
+
+function projectInfo(name, type, rate, due) {
+  var rowEl = $("<tr>");
+  var projectNameEl = $("<td>").text(name);
+  var projectTypeEl = $("<td>").text(type);
+  var projectRateEl = $("<td>").text(rate);
+  var projectDueEl = $("<td>").text(due);
+
+  var daysToDue = moment(dueDate, "MM/DD/YYYY").diff(moment(), "days");
+  var remainingDays = $("<td>").text(daysToDue);
+
+  var earnings = calculateEarnings(hourlyRate, daysToDue);
+
+  var projectTotalEl = $("<td>").text("$" + earnings);
+
+  var deleteProject = $("<td>").text("X");
+
+  rowEl.append(
+    projectNameEl,
+    projectTypeEl,
+    projectRateEl,
+    projectDueEl,
+    remainingDays,
+    projectTotalEl,
+    deleteProject
+  );
+
+  tableDisplayEl.append(rowEl);
+
+  modalEl.modal("hide");
+}
 
 // Date picker in modal.
 $(function () {
